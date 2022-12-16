4f172fc6316a637ffd8c87db251f5ac0d3c9b0e2
Ritchie Paul Buitre <ritchie@richorelse.com>
2022-12-16 10:14:52 +0800

    Install shoulda-matchers

---
+++ spec/support/shoulda.rb
@@ -0,0 +1,14 @@
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    # Keep as many of these lines as are necessary:
+    with.library :active_record
+    with.library :active_model
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
